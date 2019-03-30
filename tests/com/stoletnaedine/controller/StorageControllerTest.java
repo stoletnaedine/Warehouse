4f395c4dc1fd688cec66179ab57d89eaa52c46ca
stoletnaedine <artur.islamgulov@gmail.com>
2019-03-30 14:18:08 +0300

    init tests

---
+++ tests/com/stoletnaedine/controller/StorageControllerTest.java
@@ -0,0 +1,30 @@
+package com.stoletnaedine.controller;
+
+import org.junit.Test;
+
+public class StorageControllerTest {
+
+    @Test
+    public void checkNotNullSizeStorage() {
+    }
+
+    @Test
+    public void getRandomInt() {
+    }
+
+    @Test
+    public void getArticleBySearch() {
+    }
+
+    @Test
+    public void randomFillCells() {
+    }
+
+    @Test
+    public void getListAllArticles() {
+    }
+
+    @Test
+    public void setArticle() {
+    }
+}
