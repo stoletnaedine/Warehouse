aaade366c2a176dd56fda84f82343d679d68e811
stoletnaedine <artur.islamgulov@gmail.com>
2019-03-30 17:00:00 +0300

    test for getArticleBySearch

--- tests/com/stoletnaedine/controller/StorageControllerTest.java
+++ tests/com/stoletnaedine/controller/StorageControllerTest.java
@@ -1,6 +1,11 @@
 package com.stoletnaedine.controller;
 
+import com.stoletnaedine.model.Article;
+import com.stoletnaedine.model.Storage;
 import org.junit.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
 
 public class StorageControllerTest {
 
@@ -13,7 +18,14 @@ public class StorageControllerTest {
     }
 
     @Test
-    public void getArticleBySearch() {
+    public void testGetArticleBySearch() throws Exception {
+        Storage storage = new Storage(4,5);
+        storage.setArticle(new Point(2,4), new Article(6391, "Fireball"));
+        storage.setArticle(new Point(4,1), new Article(1234, "fIREball"));
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("2-4 | 6391 | Fireball");
+        expected.add("4-1 | 1234 | fIREball");
+        assert expected.equals(StorageController.getArticleBySearch(storage, "fire"));
     }
 
     @Test
